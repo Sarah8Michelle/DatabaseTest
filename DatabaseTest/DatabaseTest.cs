3eaa7feaadd0155a3e5c91bad439691121265cf2
sdelrosario <sdelrosario@PHUTISOF001>
2021-05-26 11:59:17 -0400

    Add project files.

---
+++ DatabaseTest/DatabaseTest.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace DatabaseTest
+{
+    public partial class DatabaseTest : ServiceBase
+    {
+        #region Declaration of instances
+        //defined to count all the times the service repeats an action.
+        private int eventId = 1;
+        //defined to set the timer that the service uses.
+        Timer timer;
+        //defined to log events that occur in the service.
+        EventLog eventLog;
+        //defined to set the source of the service and where the logs will be showing.
+        const string source = "DatabaseTestLog";
+        const string log = "Application";
+        #endregion
+
+        /// <summary>
+        /// Initialize a new instance of the DatabaseTest class.
+        /// </summary>
+        public DatabaseTest()
+        {
+            InitializeComponent();
+
+            eventLog = new EventLog();
+
+            //Checks if the event source exist in the local computer.
+            if (!EventLog.SourceExists(source))
+            {
+                //Creates a new custom log in the computer.
+                EventLog.CreateEventSource(source, log);
+            }
+
+            //Sets the source name to register and use when writing the event log.
+            eventLog.Source = source;
+
+            //Sets the name of the log to write to.
+            eventLog.Log = log;
+        }
+
+        /// <summary>
+        /// Specifies actions to take when the service starts.
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                //This timer calls the OnTimer method every 30 seconds.
+                timer = new Timer();
+                timer.Interval = 30000; // 30 seconds
+                timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+                timer.Start();
+            }
+
+            catch (Exception ex)
+            {
+                //log event in case of error.
+                eventLog.WriteEntry($"Error: {ex.Message}. Found at: {ex.StackTrace}", EventLogEntryType.Error);
+            }
+        }
+
+        /// <summary>
+        /// Specifies actions to take when the service stops.
+        /// </summary>
+        protected override void OnStop()
+        {
+            //log event.
+            eventLog.WriteEntry("In OnStop.", EventLogEntryType.Warning);
+        }
+
+        /// <summary>
+        /// This method is in charge of calling the UpdateDatabase method and log the event everytime it repeats itself.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnTimer(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                DatabaseTestService databaseTestService = new DatabaseTestService();
+
+                var time = DateTime.Now;
+
+                eventLog.WriteEntry("Checking database for updates... at time: " + time.ToString(), EventLogEntryType.Information, eventId++);
+
+                databaseTestService.UpdateDatabase();                
+            }
+
+            catch (Exception ex)
+            {
+                //log event in case of error.
+                eventLog.WriteEntry(ex.Message, EventLogEntryType.Error, 1);
+            }
+        }
+    }
+}
---
+++ DatabaseTest/DatabaseTestService.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTest
+{
+    public class DatabaseTestService
+    {
+        private string connectionString = "Data Source = Database server; Initial Catalog = Name of the database; User ID = Your user; Password = Your password; Trusted_Connection = false (true if using Windows Authentication)";
+
+        /// <summary>
+        /// This method copies the data in Table1 and paste it in Table2 of the DatabaseTest database. It also updates the IsChecked column after the copy is done.
+        /// </summary>
+        public void UpdateDatabase()
+        {
+            try
+            {
+                SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+                //Openning SQL Connection.
+                sqlConnection.Open();
+
+                using(SqlDataAdapter sqlDataAdapter = new SqlDataAdapter())
+                {
+                    /*
+                     * Summary of the code inside the 'using' statement:
+                     * First we create a DataSet to represent the in-memory cache of the data and a SqlCommand to select the columns from the first table.
+                     * The data from the first table will be saved in a temporary table called 'TablesInfo' and we will iterate through it's rows to fill the second table.
+                     * When the second table is filled, we will update the value of 'IsChecked' in the first table to complete the cycle.
+                     */
+                    DataSet dataSet = new DataSet();
+                    SqlCommand sqlCommand = new SqlCommand("SELECT [FullName], [IdNumber], [Email] FROM [dbo].[Table1] WHERE IsChecked = 0", sqlConnection);
+                    sqlDataAdapter.SelectCommand = sqlCommand;
+
+                    sqlDataAdapter.Fill(dataSet, "TablesInfo");
+                    DataTable dataTable = dataSet.Tables["TablesInfo"];
+
+                    foreach(DataRow row in dataTable.Rows)
+                    {
+                        SqlCommand insertCommand = new SqlCommand("INSERT INTO [dbo].[Table2]([FullName1], [IdNumber1], [Email1]) VALUES ('" + row[0].ToString() + "'," + row[1] + ", '" + row[2].ToString() +"')", sqlConnection);
+
+                        if(insertCommand.ExecuteNonQuery() > 0)
+                        {
+                            SqlCommand updateCommand = new SqlCommand("UPDATE [dbo].[Table1] SET IsChecked = 1 WHERE IsChecked = 0", sqlConnection);
+                            int i = updateCommand.ExecuteNonQuery();
+                        }
+                        else
+                        {
+                            EventLog.WriteEntry("DatabaseTestLog", "No rows were affected.", EventLogEntryType.Warning);
+                        }
+                    }
+
+                    //Releases all resources used by SqlDataAdapter.
+                    sqlDataAdapter.SelectCommand.Dispose();
+
+                    //Closing SQL Connection.
+                    sqlConnection.Close();                    
+                }
+            }
+            
+            catch(SqlException exception)
+            {
+                //log event.
+                EventLog.WriteEntry("DatabaseTestLog", $"SQL Error: {exception.Message}, Found at: {exception.StackTrace}", EventLogEntryType.Error);
+            }
+
+            catch (Exception exception)
+            {
+                //log event.
+                EventLog.WriteEntry("DatabaseTestLog", $"Error: {exception.Message}, Found at: {exception.StackTrace}", EventLogEntryType.Error);
+            }
+        }
+    }
+}
